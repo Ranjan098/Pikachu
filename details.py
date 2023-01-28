1def095dbb581d957e52327fd44ae49339d9ce8d
Xenomorph456 <121023268+Xenomorph456@users.noreply.github.com>
2023-01-28 20:50:14 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
-api_id = "29258160"
-api_hash = "6456101ce407f54d0a7fabb456b00b23"
-bot_token = "5271429137:AAHETo_H16mCSe69mTU_Li9d0x0WoHK2VAM"
-sudo_groups = -1001697616505
+api_id = "9541960"
+api_hash = "17a1aa509b35564481dae98d89d0191d"
+bot_token = "5815419087:AAHGG9MO5jT9SgVBdxIG0wjIfWs-sy2f7T4"
+sudo_groups = -1001532875987
